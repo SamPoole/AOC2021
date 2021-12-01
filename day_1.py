e554a32bb759659697d0841763d1c67a5338a473
Sam Poole <sam.n.poole@gmail.com>
2021-12-01 08:22:40 +0000

    Day 1

---
+++ day_1.py
@@ -0,0 +1,15 @@
+from utils import read_file
+
+input_data = [int(x) for x in read_file(1)]
+
+part_1, part_2 = 0, 0
+
+for i in range(1, len(input_data)):
+    if input_data[i] > input_data[i - 1]:
+        part_1 += 1
+
+    if i > 3:
+        if sum(input_data[i-2:i+1]) > sum(input_data[i-3:i]):
+            part_2 += 1
+
+print(f'Day 1: {part_1}, {part_2}')
